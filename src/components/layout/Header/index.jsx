88b799486b94aba6fb2997bab116833d7d92d4d7
emayom <emayom7@gmail.com>
2024-01-06 00:40:56 +0900

    :lipstick: Design: Header 컴포넌트 스타일 수정

--- src/components/layout/Header/index.jsx
+++ src/components/layout/Header/index.jsx
@@ -3,9 +3,9 @@ import * as styles from "./styles.css";
 const Header = ({ children, leftChild, rightChild }) => {
   return (
     <header className={styles.header}>
-      <div>{leftChild}</div>
-      <span style={{textAlign: 'center'}}>{children}</span>
-      <div>{rightChild}</div>
+      <div className={styles.headerBtn}>{leftChild}</div>
+      <span className={styles.headerText}>{children}</span>
+      <div className={styles.headerBtn}>{rightChild}</div>
     </header>
   );
 };
--- src/components/layout/Header/styles.css.ts
+++ src/components/layout/Header/styles.css.ts
@@ -1,14 +1,26 @@
 import { style } from "@vanilla-extract/css";
-import { border } from "../../../styles/style.css";
+import { border, flexContainer } from "../../../styles/style.css";
 
 export const header = style([
   border,
+  flexContainer.row,
   {
-    display: "grid",
-    gridTemplateColumns: "repeat(3, 1fr)",
     alignItems: "center",
     justifyContent: "space-between",
     borderBottomWidth: "1px",
     padding: "1rem 2rem",
   },
 ]);
+
+export const headerBtn = style({
+  flex: "1 0 0",
+  selectors: {
+    [`${header} &:first-child`]: { textAlign: "left" },
+    [`${header} &:last-child`]: { textAlign: "right" },
+  },
+});
+
+export const headerText = style({
+  flex: "2 1 0",
+  textAlign: "center",
+});
