24a4c469eb4795fe8e8ef445440d88b1b479b4e2
emayom <emayom7@gmail.com>
2024-01-06 01:47:36 +0900

    :lipstick: Design: Home 헤더 버튼 아이콘 추가

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -21,7 +21,7 @@ const Home = () => {
         currentDate.getMonth(),
         1
       ).getTime();
-      
+
       const LAST_DATE = new Date(
         currentDate.getFullYear(),
         currentDate.getMonth() + 1,
@@ -49,18 +49,18 @@ const Home = () => {
       <Header
         leftChild={
           <Button onClick={() => handleViewDate(-1)}>
-            <LeftChevronSVG width="24" height="24" />
+            <LeftChevronSVG width="15" height="15" />
           </Button>
         }
         rightChild={
           <Button onClick={() => handleViewDate(+1)}>
-            <RightChevronSVG width="24" height="24" />
+            <RightChevronSVG width="15" height="15" />
           </Button>
         }
       >
         {`${currentDate.getFullYear()}년 ${currentDate.getMonth() + 1}월`}
       </Header>
-      <DiaryList data={data} />
+      {data ? <DiaryList data={data} /> : <div>로딩 중 ... </div>}
     </>
   );
 };
