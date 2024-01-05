1b68abfb41cc8f5fa4c6e309075e1cc1092b8b52
emayom <emayom7@gmail.com>
2024-01-06 01:20:42 +0900

    :lipstick: Design: 버튼 아이콘 추가

--- src/components/pages/New/index.jsx
+++ src/components/pages/New/index.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import Header from "@components/layout/Header";
 import Button from "@components/Button";
 import DiaryEditor from "@components/DiaryEditor";
+import LeftChevronSVG from "@/assets/leftChevron.svg?react";
 
 const New = () => {
   const navigate = useNavigate();
@@ -10,7 +11,12 @@ const New = () => {
   return (
     <>
       <Header
-        leftChild={<Button onClick={() => navigate(-1)}>뒤로 가기</Button>}
+        leftChild={
+          <Button onClick={() => navigate(-1)}>
+            <LeftChevronSVG width="15" height="15" />
+            뒤로 가기
+          </Button>
+        }
       >
         새로운 일기 쓰기
       </Header>
