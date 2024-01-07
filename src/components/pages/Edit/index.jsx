8fc900d4c3d4700893b9eb35442383942ad580fb
emayom <emayom7@gmail.com>
2024-01-07 10:52:52 +0900

    :sparkles: Feat: handleDelete 구현 및 버튼 아이콘 추가

--- src/components/pages/Edit/index.jsx
+++ src/components/pages/Edit/index.jsx
@@ -1,17 +1,27 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useDiaryStateContext } from "@/context/DiaryProvider";
+import { useDiaryDispatchContext } from "@/context/DiaryProvider";
 
 import Header from "@components/layout/Header";
 import Button from "@components/Button";
 import DiaryEditor from "@components/DiaryEditor";
 
+import LeftChevronSVG from "@/assets/leftChevron.svg?react";
+import TrashSVG from "@/assets/trash.svg?react";
+
 const Edit = () => {
   const navigate = useNavigate();
   const { id } = useParams();
   const [origin, setOrigin] = useState();
-  
+
   const diaryList = useDiaryStateContext();
+  const { onRemove } = useDiaryDispatchContext();
+
+  const handleDelete = (targeId) => {
+    onRemove(targeId);
+    navigate("/", { replace: "true" });
+  };
 
   useEffect(() => {
     if (diaryList.length >= 1) {
@@ -30,7 +40,18 @@ const Edit = () => {
   return (
     <>
       <Header
-        leftChild={<Button onClick={() => navigate(-1)}>뒤로 가기</Button>}
+        leftChild={
+          <Button onClick={() => navigate(-1)}>
+            <LeftChevronSVG width="15" height="15" />
+            뒤로 가기
+          </Button>
+        }
+        rightChild={
+          <Button variant="negative" onClick={() => handleDelete(id)}>
+            <TrashSVG width="15" height="15" />
+            삭제하기
+          </Button>
+        }
       >
         일기 수정하기
       </Header>
