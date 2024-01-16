7e0f2f0b40611ff5b9a773f3c62d089e62d2ab06
emayom <emayom7@gmail.com>
2024-01-17 01:27:04 +0900

    :sparkles: Feat: ViewTypeProvider 추가

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import { DiaryProvider } from "./context/DiaryProvider.jsx";
+import { DiaryProvider } from "@context/DiaryProvider.jsx";
+import { ViewTypeProvider } from "@context/ViewTypeProvider.jsx";
+
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <DiaryProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ViewTypeProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ViewTypeProvider>
     </DiaryProvider>
   </React.StrictMode>
 );
